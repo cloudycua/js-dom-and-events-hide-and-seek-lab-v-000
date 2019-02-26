fb32556160bfe3484c94aed6503cc1187dbb8f72
Christine Cua <cloudycua@gmail.com>
2019-02-26 15:12:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const list = document.querySelectorAll('.ranked-list li')
+  const list = document.querySelectorAll('.ranked-list')
 
   for (let i = 0; i < list.length; i++) {
     list[i].innerHTML = (list[i].innerHTML + n).toString();
