bf7ababa656baf759225c8180fe1a0c8a18e3f91
Christine Cua <cloudycua@gmail.com>
2019-02-26 15:15:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function increaseRankBy(n) {
   const list = document.querySelectorAll('.ranked-list li')
 
   for (let i = 0; i < list.length; i++) {
-    list[i].innerHTML = (list[i].innerHTML + n).toString();
+    list[i].innerHTML = (i + n).toString();
   }
 }
